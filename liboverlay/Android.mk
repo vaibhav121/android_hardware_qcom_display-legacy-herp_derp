c8204aa0da952d4ea819d695c0970206c4ae668f
Saurabh Shah <saurshah@codeaurora.org>
2012-08-18 15:11:57 -0700

    liboverlay: Add support for MDSS Rotator.
    Acked-by: Sushil Chauhan <sushilchauhan@codeaurora.org>
    
    Change-Id: I60a2b0ce2b491d03b2b1a04966113e253b7f3dd8

--- liboverlay/Android.mk
+++ liboverlay/Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE                  := liboverlay
 LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdutils libmemalloc
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdoverlay\"
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES := \
@@ -14,6 +14,8 @@ LOCAL_SRC_FILES := \
       overlayCtrl.cpp \
       overlayUtils.cpp \
       overlayMdp.cpp \
-      overlayRotator.cpp
+      overlayRotator.cpp \
+      mdpRotator.cpp \
+      mdssRotator.cpp
 
 include $(BUILD_SHARED_LIBRARY)
