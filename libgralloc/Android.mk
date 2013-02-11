04180343143496d74f448d4c50b33c445157c8ca
Naomi Luis <nluis@codeaurora.org>
2013-02-11 12:46:24 -0800

    libgralloc: Get the stride information from libadreno_utils.so
    libadreno_utils.so is an adreno utility library. It computes the
    optimal stride required for a surface of a particular width and
    bpp. Using this computed stride allows us to efficiently use the
    dual memory channels and increase performance.
    Currently the utility library only computes the optimal stride
    for RGB surfaces.
    
    Change-Id: I0f70bd11602a5144515ab07c3bd2b80a01586a06
    
    Conflicts:
    
    	libgralloc/Android.mk

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -24,7 +24,7 @@ LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc libgenlock
 LOCAL_SHARED_LIBRARIES        += libqdutils libGLESv1_CM
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\"
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               :=  gpu.cpp gralloc.cpp framebuffer.cpp mapper.cpp
 
 include $(BUILD_SHARED_LIBRARY)
@@ -34,10 +34,10 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE                  := libmemalloc
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes)
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libgenlock libqdutils
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libgenlock libqdutils libdl
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdmemalloc\"
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               :=  ionalloc.cpp alloc_controller.cpp
 
 include $(BUILD_SHARED_LIBRARY)
