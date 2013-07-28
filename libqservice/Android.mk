bc42b234bf97957221a61e82e4c90e128ca0ab9e
RonGokhale <cips173@gmail.com>
2013-07-28 19:25:48 +0530

    display-legacy: fix
    Change-Id: Icc249b01009b153c9dcf9cec6522a0d62514c396

--- libqservice/Android.mk
+++ libqservice/Android.mk
@@ -12,7 +12,5 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := QService.cpp \
                                  IQService.cpp
 LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
-LOCAL_COPY_HEADERS            := IQService.h \
-                                 IQClient.h
 
 include $(BUILD_SHARED_LIBRARY)
